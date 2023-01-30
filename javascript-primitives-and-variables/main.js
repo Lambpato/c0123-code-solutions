7b2b4975fcb6cab2b70f6888f75fbe21899336df
Lamberto Martinez <lambpato@gmail.com>
2023-01-30 18:19:49 +0000

    Completed javascript-primitves-and-variables exercise

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,21 @@
+var fullName;
+var isCool;
+var totalPets;
+var unknown;
+var never;
+
+fullName = 'Lamberto Martinez';
+isCool = false;
+totalPets = 0;
+unknown = null;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', unknown);
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
