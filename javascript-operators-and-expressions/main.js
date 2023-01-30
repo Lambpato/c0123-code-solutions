be40df2b2ed74f836a734f02af9012c760761393
Lamberto Martinez <lambpato@gmail.com>
2023-01-30 20:11:14 +0000

    completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,45 @@
+var width = 420;
+var height = 70;
+var area = width * height;
+
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 29400;
+var payment = 1073;
+var change = bill - payment;
+
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 100;
+var midterms = 4;
+var final = 20;
+var grade = ((quizzes + midterms + final) / 3);
+
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Lamberto';
+var lastName = 'Martinez';
+var fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 12.0;
+var isAcidic = pH < 7;
+
+console.log('value of is acidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 420;
+var isSparta = headCount === 300;
+
+console.log('value of isSparta:', isSparta);
+console.log('type of isSparta:', isSparta);
+
+var motto = fullName + ' is the GOAT';
+
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
