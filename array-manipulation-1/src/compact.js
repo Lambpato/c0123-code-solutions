ab343255242750cac3c11a8ae0d7273c93c6a5e0
Lamberto Martinez <lambpato@gmail.com>
2023-02-06 03:03:45 +0000

    Downvote on the compact

---
+++ array-manipulation-1/src/compact.js
@@ -0,0 +1,25 @@
+/* exported compact */
+
+function compact(array) {
+  var unwanted = ['', null, false, -0, NaN, 0, undefined];
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    for (var y = 0; y < unwanted.length; y++) {
+      if (array[i] !== unwanted[y]) {
+        newArray.push(array[i]);
+      }
+    }
+  }
+  return newArray;
+}
+
+/*
+declare the function
+declare a an array with the list of conditions
+declare an empty variable where the wanted elements will be pushed to
+declare a for loop that'll iterate through the array parameter
+declare a for loop that'll iterate through the list of unwanted elements
+declare an if statement thatll compare both arrays to one another
+push the wanted elements to an array
+return the good array
+*/
---
+++ array-manipulation-1/src/last.js
@@ -0,0 +1,10 @@
+/* exported last */
+
+function last(array) {
+  return array[array.length - 1];
+}
+
+/*
+declare function
+return array[array.length - 1]
+*/
