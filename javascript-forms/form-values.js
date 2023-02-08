36d92241c2bc376f0e0c24cb5cc395cba43a3a08
Lamberto Martinez <lambpato@gmail.com>
2023-02-08 18:44:53 +0000

    more efficient

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -3,11 +3,11 @@ var $contact = document.querySelector('#contact-form');
 function preDefault(e) {
   e.preventDefault();
   var messgaeData = {
-    name: document.getElementById('contact-form').elements[0].value,
-    email: document.getElementById('contact-form').elements[1].value,
-    message: document.getElementById('contact-form').elements[2].value
+    name: $contact.elements[0].value,
+    email: $contact.elements[1].value,
+    message: $contact.elements[2].value
   };
   console.log(messgaeData);
-  document.getElementById('contact-form').reset();
+  $contact.reset();
 }
 $contact.addEventListener('submit', preDefault);
