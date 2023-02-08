eaf866a9ad3c59c52cd8fff070be22ca1030ece8
Lamberto Martinez <lambpato@gmail.com>
2023-02-08 21:38:27 +0000

    save

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,5 +1,15 @@
 /* exported ransomCase */
 
 function ransomCase(string) {
+  var ransom = [];
+  for (var i = 0; i < string.length; i++) {
+    if (i % 2 === 0) {
+      ransom.push(string[i].toLowerCase());
+    } else if (i % 2 !== 0) {
+      ransom.push(string[i].toUpperCase());
+    }
+  }
+  ransom = ransom.toString();
+  return ransom.replaceAll(',', '');
 
 }
