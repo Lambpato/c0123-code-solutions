23400470f86fc0c4cb1a38fe62ae4ed3fcf36578
Lamberto Martinez <lambpato@gmail.com>
2023-02-09 17:50:54 +0000

    completed dom-event-delegation exercise

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,10 @@
+var $taskList = document.querySelector('.task-list');
+function test(e) {
+  console.log('e.target:', e.target);
+  console.log('e.target.tagName:', e.target.tagName);
+  if (e.target && e.target.matches('button')) {
+    console.log('closest .task-list-item', e.target.closest('.task-list-item'));
+    e.target.closest('.task-list-item').remove();
+  }
+}
+$taskList.addEventListener('click', test);
