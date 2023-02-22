4db78993d9f95dc3cda5a66db977783e60a9e0f8
Lamberto Martinez <lambpato@gmail.com>
2023-02-22 19:36:26 +0000

    completed javascript-bank challenge

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -28,5 +28,13 @@ Bank.prototype.getAccount = function (number) {
 };
 
 Bank.prototype.getTotalAssets = function () {
+  var grandTotal = 0;
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts.length === 0) {
+      return 0;
+    }
+    grandTotal += this.accounts[i].getBalance();
+  }
 
+  return grandTotal;
 };
