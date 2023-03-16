b223fae1366c49685f27e4b39234953eac625ac6
Lamberto Martinez <lambpato@gmail.com>
2023-03-16 20:37:50 +0000

    had it return to a new line

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -2,7 +2,7 @@ import { readFile } from 'node:fs/promises';
 
 const target = process.argv.slice(2);
 
-const result = target.map(x => readFile(x, { encoding: 'utf8' }));
+const array = target.map(x => readFile(x, { encoding: 'utf8' }));
 
-Promise.all(result).then(contents => console.log(contents.toString()))
+Promise.all(array).then(contents => console.log(contents.join('\n')))
   .catch(error => console.log(error.message));
