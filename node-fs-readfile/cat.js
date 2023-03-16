6fca226f51727402f4b05a997eaa68622fc50fc6
Lamberto Martinez <lambpato@gmail.com>
2023-03-16 22:35:30 +0000

    changes have been made

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -5,5 +5,6 @@ const target = process.argv.slice(2);
 const array = target.map(x => readFile(x, { encoding: 'utf8' }));
 
 Promise.all(array)
+
   .then(contents => console.log(contents.join('\n')))
   .catch(error => console.log(error.message));
