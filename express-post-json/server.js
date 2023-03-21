724431ef2b000574902e07113dbf6c2e8cde50c4
Lamberto Martinez <lambpato@gmail.com>
2023-03-21 18:52:57 +0000

    Fixed mistakes

--- express-post-json/server.js
+++ express-post-json/server.js
@@ -2,21 +2,21 @@ import express from 'express';
 
 const app = express();
 
-const nextId = 1;
+let nextId = 1;
 
 const grades = {};
 
-app.get('/api/grades', (req, res, next) => {
-  res.json(Object.values(grades));
-  next();
-});
-
 app.use(express.json());
+
+app.get('/api/grades', (req, res) => {
+  res.json(Object.values(grades));
+});
 
 app.post('/api/grades', (req, res) => {
   grades[nextId] = req.body;
   grades[nextId].id = nextId;
-  res.status(201).send(grades);
+  nextId++;
+  res.status(201).send(req.body);
 });
 
 app.listen(8080, () => {
