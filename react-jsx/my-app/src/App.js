e28ef745951e125749e77f34522880d4941f574d
Lamberto Martinez <lambpato@gmail.com>
2023-03-27 21:34:51 +0000

    please click on the link on App.js

---
+++ react-jsx/my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=oHg5SJYRHA0"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          CLICK ME! NOW!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
