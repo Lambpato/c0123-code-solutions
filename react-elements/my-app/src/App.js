26d6d115309f993f580cbc78befbd0252aeb1ba6
Lamberto Martinez <lambpato@gmail.com>
2023-03-27 21:14:56 +0000

    completed react-elements exericse

--- react-elements/my-app/src/App.js
+++ react-elements/my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn to Code
+          Learn to Code with LearningFuze
         </a>
       </header>
     </div>
