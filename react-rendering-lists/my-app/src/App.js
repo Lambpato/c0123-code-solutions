dbebe7e55b62af4302768c58f5e7ff87ea1a9a3c
Lamberto Martinez <lambpato@gmail.com>
2023-03-30 01:07:34 +0000

    completed react-rendering-lists exercise

---
+++ react-rendering-lists/my-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import PokemonList from './PokemonList';
+
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      <PokemonList pokedex={pokedex} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
