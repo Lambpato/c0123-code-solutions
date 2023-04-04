bacf6a46efb5ec19ff5946a03d932eba0ab17058
Lamberto Martinez <lambpato@gmail.com>
2023-04-04 16:48:31 +0000

    added and corrected error handling

--- fetch/main.js
+++ fetch/main.js
@@ -1,13 +1,9 @@
 const logJSONData = async () => {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!response.ok) throw new Error(`Error Code: ${response.status} Error Message: It Broken`);
     const dataJson = await response.json();
     console.log(dataJson);
-
-    if (!response.ok) {
-      throw new Error(`Error Code: ${response.status} Error Message: It Broken`);
-    }
-
   } catch (err) {
     console.error('Error:', err.message);
   }
@@ -18,6 +14,7 @@ logJSONData();
 const logPokeData = async () => {
   try {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu');
+    if (!response.ok) throw new Error(`Error Code: ${response.status} Error Message: It Broken`);
     const dataJson = await response.json();
     console.log(dataJson);
   } catch (err) {
