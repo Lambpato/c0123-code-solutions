62751361535a1d653c7ae7e45d183f8043d3c1d1
Lamberto Martinez <lambpato@gmail.com>
2023-04-07 21:39:07 +0000

    made line 27 cleaner

--- react-custom-hooks/client/src/components/useTodos.js
+++ react-custom-hooks/client/src/components/useTodos.js
@@ -24,7 +24,7 @@ export function useTodos() {
 
   const addTodo = async (newTodo) => {
     const nuevo = await createTodo(newTodo);
-    setTodos((prev) => prev.concat(nuevo));
+    setTodos([...todos, nuevo]);
   }
 
 
