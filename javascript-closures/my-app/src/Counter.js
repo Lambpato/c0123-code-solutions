148c802d11d845c7806384d9bcb8d8db812a1b75
Lamberto Martinez <lambpato@gmail.com>
2023-07-22 06:18:55 +0000

    wrapped getData funtion in a useCallback

--- javascript-closures/my-app/src/Counter.js
+++ javascript-closures/my-app/src/Counter.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 
 export default function Counter() {
   const [counter, setCounter] = useState(0);
+
+  const getData = useCallback(() => {
+    return { foo: 'bar' };
+  }, []);
+
   useEffect(() => {
     getData();
-    setCounter((prev) => prev + 1);
+    setCounter(prev => prev + 1);
   }, [getData]);
-
-  function getData() {
-    // fetch data
-    return { foo: 'bar' };
-  }
 
   return <div>Fetching {counter} times</div>;
 }
