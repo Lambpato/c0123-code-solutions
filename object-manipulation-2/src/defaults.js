2dbfe5b9df461df79d0e02ed9f2cf74b4e3e96c5
Lamberto Martinez <lambpato@gmail.com>
2023-07-23 19:01:47 +0000

    compelted object-manipulation-2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,3 +1,9 @@
 /* exported defaults */
 
-function defaults(target, source) {}
+function defaults(target, source) {
+  for (const key in source) {
+    if (target[key] === undefined) {
+      target[key] = source[key];
+    }
+  }
+}
