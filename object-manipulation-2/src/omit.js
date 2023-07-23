a7d62b285246d50abf8c2377c68be6481cb1be96
Lamberto Martinez <lambpato@gmail.com>
2023-07-23 16:07:29 +0000

    began working on omit

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,12 @@
 /* exported omit */
+
+function omit(source, keys) {
+  let object = {};
+  object = source;
+  for (const prop in source) {
+    if (keys.includes(prop) && source[prop] !== undefined) {
+      object[prop] = source[prop];
+    }
+  }
+  return object;
+}
