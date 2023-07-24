97ba07a3a9497702ce844632fb093612b917e841
Lamberto Martinez <lambpato@gmail.com>
2023-07-24 15:16:36 +0000

    made isPalindromic to an arrow function

--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -1,7 +1,7 @@
 /* exported isPalindromic */
 
-function isPalindromic(string) {
+const isPalindromic = string => {
   const gnirts =
     string === 'taco cat' ? 'taco cat' : string.split('').reverse().join('');
   return gnirts === string;
-}
+};
