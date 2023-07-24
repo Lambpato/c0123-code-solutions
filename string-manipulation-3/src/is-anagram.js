a2610ab43d39d4e243e39fb9cafe6a1d6f7f833d
Lamberto Martinez <lambpato@gmail.com>
2023-07-24 16:54:03 +0000

    still working on isAnagram, saving progress

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,3 +1,14 @@
 /* exported isAnagram */
 
-function isAnagram(firstString, secondString) {}
+function isAnagram(firstString, secondString) {
+  const fString = firstString.split('').sort();
+  const sString = secondString.split('').sort();
+  console.log(fString);
+  console.log(sString);
+  for (let i = 0; i < fString.length; i++) {
+    if (fString[i] !== sString[i]) {
+      return false;
+    }
+  }
+  return true;
+}
