a4686c9bd5e1878698c706c0e27c15660113383d
Lamberto Martinez <lambpato@gmail.com>
2023-08-06 02:27:11 +0000

    removed console logs from is anagram

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -3,8 +3,6 @@
 function isAnagram(firstString, secondString) {
   const arr1 = firstString.replaceAll(' ', '').split('').sort();
   const arr2 = secondString.replaceAll(' ', '').split('').sort();
-  console.log(arr1);
-  console.log(arr2);
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
