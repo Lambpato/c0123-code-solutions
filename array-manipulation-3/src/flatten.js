ff89ef4867df77e103780c6f297640bfa5166521
Lamberto Martinez <lambpato@gmail.com>
2023-08-07 23:33:44 +0000

    flatten now works

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,3 +1,15 @@
 /* exported flatten */
 
-const flatten = array => {};
+const flatten = array => {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let o = 0; o < array[i].length; o++) {
+        newArray.push(array[i][o]);
+      }
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+};
