fab76f3c97af44af4f22f10f2d79e4457f91d4f4
Lamberto Martinez <lambpato@gmail.com>
2023-08-08 00:26:29 +0000

    Completed array-manipulation-3

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,3 +1,13 @@
 /* exported intersection */
 
-const intersection = (first, second) => {};
+const intersection = (first, second) => {
+  const newArr = [];
+  for (let i = 0; i < first.length; i++) {
+    for (let o = 0; o < second.length; o++) {
+      if (second[o] === first[i]) {
+        newArr.push(second[o]);
+      }
+    }
+  }
+  return newArr;
+};
