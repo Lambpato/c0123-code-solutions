1bba7ddc6839358749648fe998166e0a84580f04
Lamberto Martinez <lambpato@gmail.com>
2023-08-08 00:17:47 +0000

    difference now works

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,3 +1,18 @@
 /* exported difference */
 
-const difference = (first, second) => {};
+const difference = (first, second) => {
+  const newArr = [];
+  for (let i = 0; i < first.length; i++) {
+    if (second.includes(first[i])) {
+      continue;
+    }
+    newArr.push(first[i]);
+  }
+  for (let o = 0; o < second.length; o++) {
+    if (first.includes(second[o])) {
+      continue;
+    }
+    newArr.push(second[o]);
+  }
+  return newArr;
+};
