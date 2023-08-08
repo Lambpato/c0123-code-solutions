6ebd7a96d674465a69e5b1482390ffb53e341a97
Lamberto Martinez <lambpato@gmail.com>
2023-08-08 00:22:28 +0000

    union now works

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,3 +1,14 @@
 /* exported union */
 
-const union = (first, second) => {};
+const union = (first, second) => {
+  const newArr = [];
+  for (let i = 0; i < first.length; i++) {
+    newArr.push(first[i]);
+  }
+  for (let o = 0; o < second.length; o++) {
+    if (!newArr.includes(second[o])) {
+      newArr.push(second[o]);
+    }
+  }
+  return newArr;
+};
